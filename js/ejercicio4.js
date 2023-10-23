f9d2011b47cd2840fe21cea3ce97704c4c182305
Johan Zamudio <zamudiojohan0@gmail.com>
2023-10-22 22:37:25 -0300

    Co-authored-by: Favio-Richar <Favio-Richar@users.noreply.github.com>

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -2,10 +2,11 @@
     
     Num1 = prompt("Ingrese un numero entre 0 y 100");
 
-    for(var i = 0; i < Num1.length; i++){
+    for(var i = 0; i <= Num1; i++){
 
 
-        document.write(i);
+        
+
 
     }
 
